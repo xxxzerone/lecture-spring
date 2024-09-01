4a60da21d981df1c59b10d9237752029199f0a3b
xxxzerone <enpunk90@gmail.com>
2024-09-02 02:03:11 +0900

    1

---
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,30 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+@Data
+public class Item {
+
+//    @NotNull(groups = UpdateCheck.class)
+    private Long id;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1_000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class})
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
